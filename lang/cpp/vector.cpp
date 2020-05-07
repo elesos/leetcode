860b7f12527535fa06a4f462146aa6fa30bfa76c
elesos <hnrayer@gmail.com>
2020-05-07 09:18:00 +0800

    update by elesos.com

--- lang/cpp/vector.cpp
+++ lang/cpp/vector.cpp
@@ -1,6 +1,6 @@
 // C++ program to illustrate the 
 // iterators in vector 
-#include <iostream> 
+/* #include <iostream> 
 #include <vector> 
 
 using namespace std; 
@@ -16,10 +16,13 @@ int main()
 	for (auto i = g1.begin(); i != g1.end(); ++i) 
 		cout << *i << " "; 
 
+	//c代表常量的
 	cout << "\nOutput of cbegin and cend: "; 
 	for (auto i = g1.cbegin(); i != g1.cend(); ++i) 
 		cout << *i << " "; 
 
+	
+	
 	cout << "\nOutput of rbegin and rend: "; 
 	for (auto ir = g1.rbegin(); ir != g1.rend(); ++ir) 
 		cout << *ir << " "; 
@@ -29,4 +32,56 @@ int main()
 		cout << *ir << " "; 
 
 	return 0; 
+}  */
+
+//=============================Capacity===================
+// C++ program to illustrate the 
+// capacity function in vector 
+#include <iostream> 
+#include <vector> 
+
+using namespace std; 
+
+int main() 
+{ 
+	vector<int> g1; 
+
+	for (int i = 1; i <= 5; i++) 
+		g1.push_back(i); 
+
+	cout << "Size : " << g1.size(); 
+	cout << "\nCapacity : " << g1.capacity(); 
+	cout << "\nMax_Size : " << g1.max_size(); 
+
+	for (auto it = g1.begin(); it != g1.end(); it++) 
+		cout << *it << " "; 
+	
+	
+	// resizes the vector size to 4 
+	g1.resize(4); 
+
+	// prints the vector size after resize() 
+	cout << "\nSize : " << g1.size(); 
+
+	for (auto it = g1.begin(); it != g1.end(); it++) 
+		cout << *it << " "; 
+	
+	
+	
+	// checks if the vector is empty or not 
+	if (g1.empty() == false) 
+		cout << "\nVector is not empty"; 
+	else
+		cout << "\nVector is empty"; 
+
+	// Shrinks the vector 
+	g1.shrink_to_fit(); 
+	cout << "\nVector elements are: "; 
+	for (auto it = g1.begin(); it != g1.end(); it++) 
+		cout << *it << " "; 
+
+	return 0; 
 } 
+
+
+
